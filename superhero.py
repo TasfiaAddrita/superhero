8623f4eb0fc9bcb8f0cd95b6adc54fb6aee9bff2
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-09-11 14:40:57 -0700

    init repo

---
+++ superhero.py
@@ -0,0 +1,35 @@
+class Ability:
+    def __init__(name, max_damage):
+        pass
+
+    def attack():
+        pass
+
+class Armor:
+    def __init__(name, max_block):
+        pass
+
+    def block():
+        pass
+
+class Hero:
+    def __init__(name, starting_health):
+        pass
+
+    def add_ability(ability):
+        pass
+
+    def attack():
+        pass
+
+    def defend(incoming_damage):
+        pass
+
+    def take_damage(damage):
+        pass
+
+    def is_alive():
+        pass
+
+    def fight(opponent):
+        pass
