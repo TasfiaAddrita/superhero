79a706e5f092053f8c1cbb22ee0cd14acb9c9083
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-09-14 15:02:43 -0700

    built out ability, armor, and hero functions

--- superhero.py
+++ superhero.py
@@ -4,7 +4,7 @@ class Ability:
     def __init__(self, name, max_damage):
         self.name = name
         self.max_damage = max_damage
-        pass
+        # pass
 
     def attack(self):
         return random.randint(0, self.max_damage)
@@ -29,31 +29,82 @@ class Hero:
         self.abilities.append(ability)
 
     def attack(self):
-        pass
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
 
-    def defend(self, incoming_damage):
-        pass
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def defend(self):
+        total_blocks = 0
+        for armor in self.armors:
+            total_blocks += armor.block()
+        return total_blocks
 
     def take_damage(self, damage):
-        pass
+        self.current_health -= damage - self.defend()
 
     def is_alive(self):
-        pass
+        if self.current_health > 0:
+            return True
+        else:
+            return False
 
     def fight(self, opponent):
-        pass
+        if len(self.abilities) == 0 and len(opponent.abilities):
+            return print("Draw!")
+        while self.is_alive() and opponent.is_alive():
+            self.take_damage(opponent.attack())
+            opponent.take_damage(self.attack())
+        if self.is_alive() == False:
+            return print(f"{opponent.name} won!")
+        else:
+            return print(f"{self.name} won!")
 
 if __name__ == "__main__":
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+    # ability = Ability("Debugging Ability", 20)
+    # print(ability.name)
+    # print(ability.attack())
+    #
+    # my_hero = Hero("Grace Hopper", 200)
+    # print(my_hero.name)
+    # print(my_hero.current_health)
+    # my_hero.add_ability(ability)
+    # print(my_hero.abilities)
+    #
+    # ability2 = Ability("Flying", 50)
+    # my_hero.add_ability(ability2)
+    # print(my_hero.abilities)
 
-    my_hero = Hero("Grace Hopper", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
-    my_hero.add_ability(ability)
-    print(my_hero.abilities)
+    # ability = Ability("Great Debugging", 50)
+    # another_ability = Ability("Smarty Pants", 90)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # hero.add_ability(another_ability)
+    # print(hero.attack())
 
-    ability2 = Ability("Flying", 50)
-    my_hero.add_ability(ability2)
-    print(my_hero.abilities)
+    # hero = Hero("Grace Hopper", 200)
+    # shield = Armor("Shield", 50)
+    # hero.add_armor(shield)
+    # hero.take_damage(50)
+    # print(hero.current_health)
+
+    # hero = Hero("Grace Hopper", 200)
+    # hero.take_damage(150)
+    # print(hero.is_alive())
+    # hero.take_damage(15000)
+    # print(hero.is_alive())
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
